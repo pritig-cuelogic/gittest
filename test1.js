d59cd26e1f84677cd04f5fd93854d8a3f540a36a
pritig-cuelogic <priti.gupta@cuelogic.co.in>
2016-09-01 17:29:07 +0530

    exercise 1

---
+++ test1.js
@@ -0,0 +1,40 @@
+// 1. Write a program to create variables of the following types:
+
+var i1 = 10;
+
+i1 = 'priti';
+
+I1 = 'neha';
+ 
+i1 = true;
+console.log(I1);
+var a1 = [true,5,"priti"];
+
+a1.push(9);
+
+console.log(a1.toString());
+
+a1.pop();
+
+a1.push("test");
+ 
+a1.shift();
+
+a1.unshift("test111");
+
+console.log(a1.toString());
+
+console.log(a1.indexOf(5));
+
+var k1 = true;
+
+k2 = false;
+ 
+console.log("before toggele k1 = "+k1 + "before toggele k2 = "+k2);
+temp = k1;
+
+k1 = k2;
+
+k2 = temp;
+
+console.log("after toggele k1 = "+k1 + "after toggele k2 = "+k2);

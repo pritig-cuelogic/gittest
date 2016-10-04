afc3d237d8788ddd8fdba08e629bda4a5567b849
pritig-cuelogic <priti.gupta@cuelogic.co.in>
2016-10-04 12:13:35 +0530

    change

--- mytest.js
+++ mytest.js
@@ -1,4 +1,4 @@
-var exp = {
+/*var exp = {
 "name": "User exp",
 "msg": "provide right input"
 };
@@ -20,11 +20,11 @@ console.log(devideNumber(3,89));
 }
 catch(e){
 console.log(e);
-}
+}*/
 
 // --------------------------------------------------------------------------------------------
 
-function printNum(a){
+/*function printNum(a){
 console.log("test1"+a);
 }
 
@@ -36,4 +36,54 @@ printNum(5);
 
 var a = undefined;
 
-console.log(typeof a);
+console.log(typeof a);*/
+//-------------------------------------------------------------------------------------------------
+
+/*function Person(name){
+
+this.name = name;
+
+this.getName = function(){
+console.log("my name is: "+this.name);
+}
+}
+
+var p1 = new Person("Priti");
+var p2 = new Person("riya");
+
+p2.getName(); */
+
+//---------------------------------------------------------------------------------------------------
+
+/*var s1 = "priti";
+var s2 = new String("priti");
+var len11 = s1.length;
+console.log(typeof s1);
+console.log("test string "+(s1==s2)); */
+
+//--------------------------------------------------------------------------------
+
+/*function test1(name, callback){
+console.log("name ....");
+var a=5,b=7;
+console.log(a+b);
+callback();
+}
+
+test1("aarti",function(){
+ console.log("callback function");
+});*/
+
+//-------------------------------------------------------------------------------------------------
+
+/*function test2(a){
+  return function n1(){
+    return a*2;
+   }
+ 
+}
+
+var t1 = test2(9);
+console.log(t1());*/
+
+;
--- ulnli.js
+++ ulnli.js
@@ -1,4 +1,15 @@
-function clickLi(text){
+/*function clickLi(text){
 	alert(text);
 
-}
+}*/
+
+window.onload = function() {
+    var myLi = document.getElementById('ultag').addEventListener("click",function(e) {
+       
+        if(e.target && e.target.nodeName == "LI") {
+            alert(e.target.innerHTML );
+        }
+    });
+};
+
+
